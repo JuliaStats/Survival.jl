5028b2a657d51a7b05af60030e0fd28203a9e951
autodocs <autodocs>
2017-04-15 01:37:26 +0000

    build based on de0383f

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Survival.jl",
     "category": "section",
-    "text": "This package provides types and methods for performing survival analysis in Julia.Pages = [\n    \"km.md\",\n]\nDepth = 1"
+    "text": "This package provides types and methods for performing survival analysis in Julia."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "The package is not yet registered in Julia's package registry, and so it must be installed usingPkg.clone(\"https://github.com/ararslan/Survival.jl.git\")"
+},
+
+{
+    "location": "index.html#Contents-1",
+    "page": "Home",
+    "title": "Contents",
+    "category": "section",
+    "text": "Pages = [\n    \"km.md\",\n]\nDepth = 1"
 },
 
 {
@@ -29,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Kaplan-Meier",
     "title": "Kaplan-Meier estimator",
     "category": "section",
-    "text": "The Kaplan-Meier estimator is a nonparametric estimator of the survivor function, i.e. the probability of survival beyond a given time.The estimate is given by hatS(t) = prod_i t_i  t left( 1 - fracd_in_i right) where d_i is the number of observed events at time t_i and n_i is the number of subjects at risk for the event just before time t_i.The pointwise standard error of the log of the survivor function can be computed using Greenwood's formula: textSE(log hatS(t)) = sqrtsum_i t_i  t fracd_in_i (n_i - d_i)"
+    "text": "The Kaplan-Meier estimator is a nonparametric estimator of the survivor function, i.e. the probability of survival beyond a given time.The estimate is given byhatS(t) = prod_i t_i  t left( 1 - fracd_in_i right)where d_i is the number of observed events at time t_i and n_i is the number of subjects at risk for the event just before time t_i.The pointwise standard error of the log of the survivor function can be computed using Greenwood's formula:textSE(log hatS(t)) = sqrtsum_i t_i  t fracd_in_i (n_i - d_i)"
 },
 
 {
@@ -37,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Kaplan-Meier",
     "title": "API",
     "category": "section",
-    "text": "Survival.KaplanMeier\nSurvival.fit(::Type{Survival.KaplanMeier}, ::Any, ::Any)\nSurvival.confint(::Survival.KaplanMeier, ::Any)"
+    "text": "CurrentModule = SurvivalSurvival.KaplanMeier\nStatsBase.fit(::Type{Survival.KaplanMeier}, ::Any, ::Any)\nStatsBase.confint(::Survival.KaplanMeier, ::Any)"
 },
 
 {
