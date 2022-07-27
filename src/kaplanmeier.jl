f955580ad59b18e887b197733aceb83a274271c3
Alex Arslan <ararslan@comcast.net>
2022-07-26 21:01:12 -0700

    Allow non-`Float64` results from estimators
    Previously, estimators' estimates and standard errors were stored as
    vectors of `Float64`. Intermediate computations didn't pay much
    attention to promotion or preserving any particular type; they didn't
    have to because `Float64` was hard-coded.
    
    Now, estimators are parameterized on both the type of the time values
    (as they were previously) and the type of the estimates and standard
    errors. Importantly, the type parameter for the latter comes BEFORE the
    former, making this a subtly breaking change, albeit one that seems
    unlikely to affect most people. Indeed, the type parameters had
    previously been entirely undocumented AFAICT.
    
    Using the existing, documented pattern of e.g. `fit(KaplanMeier, ...)`
    will still produce `Float64` estimates, but one may now specifically
    request another type, e.g. `fit(KaplanMeier{Float32}, ...)`.

--- src/kaplanmeier.jl
+++ src/kaplanmeier.jl
@@ -1,29 +1,36 @@
 """
-    KaplanMeier
+    KaplanMeier{S,T}
 
 An immutable type containing survivor function estimates computed
 using the Kaplan-Meier method.
 The type has the following fields:
 
 * `events`: An [`EventTable`](@ref) summarizing the times and events
-  used to comute the estimates
-* `survival`: Estimate of the survival probability at each time
-* `stderr`: Standard error of the log survivor function at each time
+  used to compute the estimates. The time values are of type `T`.
+* `survival`: Estimate of the survival probability at each time. Values
+  are of type `S`.
+* `stderr`: Standard error of the log survivor function at each time.
+  Values are of type `S`.
 
-Use `fit(KaplanMeier, ...)` to compute the estimates and construct
-this type.
+Use `fit(KaplanMeier, ...)` to compute the estimates as `Float64`
+values and construct this type.
+Alternatively, `fit(KaplanMeier{S}, ...)` may be used to request a
+particular value type `S` for the estimates.
 """
-struct KaplanMeier{T} <: NonparametricEstimator
+struct KaplanMeier{S,T} <: NonparametricEstimator
     events::EventTable{T}
-    survival::Vector{Float64}
-    stderr::Vector{Float64}
+    survival::Vector{S}
+    stderr::Vector{S}
 end
 
-estimator_start(::Type{KaplanMeier}) = 1.0  # Estimator starting point
-stderr_start(::Type{KaplanMeier}) = 0.0 # StdErr starting point
+estimator_eltype(::Type{<:KaplanMeier{S}}) where {S} = S
+estimator_eltype(::Type{KaplanMeier}) = Float64
 
-estimator_update(::Type{KaplanMeier}, es, dᵢ, nᵢ) = es * (1 - dᵢ / nᵢ) # Estimator update rule
-stderr_update(::Type{KaplanMeier}, gw, dᵢ, nᵢ) = gw + dᵢ / (nᵢ * (nᵢ - dᵢ)) # StdErr update rule
+estimator_start(T::Type{<:KaplanMeier}) = oneunit(estimator_eltype(T))
+stderr_start(T::Type{<:KaplanMeier}) = zero(estimator_eltype(T))
+
+estimator_update(::Type{<:KaplanMeier}, es, dᵢ, nᵢ) = es * (1 - dᵢ // nᵢ)
+stderr_update(::Type{<:KaplanMeier}, gw, dᵢ, nᵢ) = gw + dᵢ // (nᵢ * (nᵢ - dᵢ))
 
 """
     confint(km::KaplanMeier; level=0.05)
