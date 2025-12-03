8506b6b5f3623ff4b325ca8b6b4644b0ec7e75d4
David Müller-Widmann <devmotion@users.noreply.github.com>
2025-12-03 11:19:46 +0100

    Use zero `delta_min`

--- src/cox.jl
+++ src/cox.jl
@@ -209,7 +209,7 @@ function _coxph(X::AbstractArray{T}, s::AbstractVector; l2_cost, tol) where T
     c = CoxAux(X, s, l2_cost)
     β₀ = zeros(R, size(X, 2))
     fgh! = NLSolversBase.TwiceDifferentiable(NLSolversBase.only_fgh!((f, G, H, x)->_cox_fgh!(x, G, H, c)), β₀)
-    optim_alg = Optim.NewtonTrustRegion()
+    optim_alg = Optim.NewtonTrustRegion(; delta_min = 0.0)
     optim_options = Optim.Options(; g_abstol = tol)
     optim_state = Optim.initial_state(optim_alg, optim_options, fgh!, β₀)
     res = Optim.optimize(fgh!, β₀, optim_alg, optim_options, optim_state)
